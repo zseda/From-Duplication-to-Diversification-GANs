015f79f2dc9a3c9e8b9607532a3e6f9151754d30
Zeynep Seda Birinci <zseda@gmail.com>
2023-10-30 10:26:31 +0100

    test discriminator and generator

---
+++ src/test architecture/test.py
@@ -0,0 +1,24 @@
+import torch
+from models.generator import Generator
+from models.discriminator import Discriminator
+
+
+def main():
+    # Test Generator
+    G = Generator()
+    dummy_img = torch.rand(size=(2, 3, 224, 224))
+    dummy_noise = torch.rand(size=(2, 112, 14, 14))
+    gen_output = G(dummy_img, dummy_noise)
+
+    print("Generator output shape:", gen_output.shape)
+
+    # Test Discriminator
+    D = Discriminator()
+    dummy_output = torch.rand(size=(2, 3, 224, 224))
+    disc_output = D(dummy_output)
+
+    print("Discriminator output shape:", disc_output.shape)
+
+
+if __name__ == "__main__":
+    main()
