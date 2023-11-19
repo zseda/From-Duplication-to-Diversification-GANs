ed547d461bdb93c915815677d81153ccd2d74067
Zeynep Seda Birinci <zseda@gmail.com>
2023-11-19 21:53:24 +0100

    bugfix discriminator

--- src/models/_discriminator.py
+++ src/models/_discriminator.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch.nn.functional as F
 import timm
 
 
@@ -9,25 +8,9 @@ class Discriminator(nn.Module):
 
         # Base model for feature extraction
         self.base_model = timm.create_model(
-            "resnet18",
-            pretrained=True,
-            features_only=True,
-            out_indices=[3],
-        )
-
-        # Classifier head
-        self.head = nn.Sequential(
-            # Flatten the output from base model
-            nn.Flatten(),
-            # Lazy fully connected layer
-            nn.LazyLinear(out_features=128),
-            nn.LeakyReLU(0.2, inplace=True),
-            # Output layer for binary classification (real vs fake)
-            nn.Linear(in_features=128, out_features=1),
-            nn.Sigmoid(),
+            "efficientnet_b0", pretrained=True, num_classes=1
         )
 
     def forward(self, x):
         x = self.base_model(x)[0]
-        x = self.head(x)
         return x
