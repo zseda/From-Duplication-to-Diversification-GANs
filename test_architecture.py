d303693a8af9d99f884b4046022bd4690d32f675
Zeynep Seda Birinci <zseda@gmail.com>
2023-11-19 21:07:52 +0100

    test CIFAR

--- test_architecture.py
+++ test_architecture.py
@@ -5,16 +5,17 @@ from src.models import Discriminator
 
 def main():
     # Test Generator
-    G = Generator()
-    dummy_img = torch.rand(size=(2, 3, 224, 224))
-    dummy_noise = torch.rand(size=(2, 112, 14, 14))
+    G = Generator("cpu")
+    dummy_img = torch.rand(size=(2, 3, 32, 32))
+
+    dummy_noise = torch.rand(size=(2, 56, 2, 2))
     gen_output = G(dummy_img, dummy_noise)
 
     print("Generator output shape:", gen_output.shape)
 
     # Test Discriminator
     D = Discriminator()
-    dummy_output = torch.rand(size=(2, 3, 224, 224))
+    dummy_output = torch.rand(size=(2, 3, 32, 32))
     disc_output = D(dummy_output)
 
     print("Discriminator output shape:", disc_output.shape)
