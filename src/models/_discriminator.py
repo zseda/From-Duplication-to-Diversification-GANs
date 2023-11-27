68ff18964299d3ce3195a99eb36716602dabc996
Zeynep Seda Birinci <zseda@gmail.com>
2023-11-27 13:33:30 +0100

    edgenet as discriminator

--- src/models/_discriminator.py
+++ src/models/_discriminator.py
@@ -9,7 +9,12 @@ class Discriminator(nn.Module):
 
         # Base model for feature extraction
         self.base_model = timm.create_model(
-            "efficientnet_b0", pretrained=True, num_classes=1
+            # "efficientnet_b0", pretrained=False, num_classes=1
+            # "efficientnet_b1", pretrained=False, num_classes=1
+            "edgenext_xx_small",
+            pretrained=False,
+            num_classes=1
+            # "eca_nfnet_l0", pretrained=False, num_classes=1
         )
 
     def forward(self, x):
