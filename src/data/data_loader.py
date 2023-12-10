3682ad0cb6b7a61109f83a3e4374ee08f9ab9eed
Zeynep Seda Birinci <zseda@gmail.com>
2023-12-10 18:00:12 +0100

    single class data loader

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,30 +1,57 @@
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 
 
-def get_cifar10_dataloader(batch_size=64, num_workers=2, download_path="./data"):
+class CIFAR10SingleClass(Dataset):
     """
-    Returns CIFAR-10 data loaders.
+    A custom CIFAR-10 dataset that only includes images from a single target class.
+    """
+
+    def __init__(self, cifar10_dataset, target_class):
+        self.cifar10_dataset = cifar10_dataset
+        self.target_class = target_class
+        self.indices = [
+            i
+            for i, (_, label) in enumerate(self.cifar10_dataset)
+            if label == self.target_class
+        ]
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        return self.cifar10_dataset[self.indices[idx]]
+
+
+def get_cifar10_dataloader(
+    target_class, batch_size=64, num_workers=2, download_path="./data"
+):
+    """
+    Returns CIFAR-10 data loaders for a specific class.
     Args:
+    - target_class (int): The class to filter out. Integer from 0 to 9.
     - batch_size (int): Batch size.
-    - num_workers (int): Number of subprocesses to use for data loading
-    - download_path (str): Directory to download CIFAR-10 dataset
+    - num_workers (int): Number of subprocesses to use for data loading.
+    - download_path (str): Directory to download CIFAR-10 dataset.
 
     Returns:
-    - train_loader: DataLoader for the training set
-    - test_loader: DataLoader for the test set
+    - train_loader: DataLoader for the training set of the specified class.
+    - test_loader: DataLoader for the test set of the specified class.
     """
 
     transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
 
-    train_dataset = datasets.CIFAR10(
+    full_train_dataset = datasets.CIFAR10(
         root=download_path, train=True, download=True, transform=transform
     )
-    test_dataset = datasets.CIFAR10(
+    full_test_dataset = datasets.CIFAR10(
         root=download_path, train=False, download=True, transform=transform
     )
+
+    train_dataset = CIFAR10SingleClass(full_train_dataset, target_class)
+    test_dataset = CIFAR10SingleClass(full_test_dataset, target_class)
 
     train_loader = DataLoader(
         train_dataset,
