c5f09f8575aa480f8b742651803170c29b966b03
Zeynep Seda Birinci <zseda@gmail.com>
2023-12-10 22:34:10 +0100

    bugfix typo init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from ._discriminator import Discriminator
 from ._generator import Generator
-from .model_dcgan import Generator as DCGAN
+from .model_dcgan import Generator as DCGenerator
 from .model_dcgan import Discriminator as DCDiscriminator
