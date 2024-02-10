b27713b3b61aa2b9e4c69d84906c772e3b160125
Zeynep Seda Birinci <zseda@gmail.com>
2024-02-10 20:31:44 +0100

    reduce params of discriminator

--- src/models/_discriminator.py
+++ src/models/_discriminator.py
@@ -30,16 +30,18 @@ class DiscriminatorCustom(nn.Module):
         super().__init__()
 
         self.pipeline = nn.Sequential(
-            StackedDecodingModule(3, 32, norm_layer=nn.BatchNorm2d),
+            StackedDecodingModule(3, 16, norm_layer=nn.BatchNorm2d),  # Reduced channels
+            StackedDecodingModule(16, 32, norm_layer=nn.BatchNorm2d),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Apply pooling earlier
             StackedDecodingModule(32, 64, norm_layer=nn.BatchNorm2d),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 16x16
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Apply additional pooling
             StackedDecodingModule(64, 128, norm_layer=nn.BatchNorm2d),
-            StackedDecodingModule(128, 256, norm_layer=nn.BatchNorm2d),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 8x8
-            StackedDecodingModule(256, 512, norm_layer=nn.BatchNorm2d),
-            StackedDecodingModule(512, 256, norm_layer=nn.BatchNorm2d),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Apply additional pooling
+            StackedDecodingModule(
+                128, 64, norm_layer=nn.BatchNorm2d
+            ),  # Reduced channels
             nn.Flatten(),
-            nn.Linear(256 * 8 * 8, 1),
+            nn.Linear(64 * 4 * 4, 1),  # Reduced input size due to additional pooling
         )
 
     def forward(self, x):
