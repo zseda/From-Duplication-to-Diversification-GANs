622bd94804a859d737c4fa9dd13ccdabe11bb0b4
Zeynep Seda Birinci <zseda@gmail.com>
2024-02-11 18:30:25 +0100

    bugfix: dataloader,optimization

--- plain_training.py
+++ plain_training.py
@@ -14,7 +14,7 @@ app = Typer()
 
 
 class GAN(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, batch_size):
 
         super(GAN, self).__init__()
 
@@ -22,92 +22,97 @@ class GAN(pl.LightningModule):
         self.generator = Generator(self.device).to(self.device)
         self.discriminator = Discriminator().to(self.device)
         self.target_class = 4
+        self.batch_size = batch_size
         # Initialize generator with dummy inputs to set up layers
         dummy_noise = torch.rand(size=(1, 56, 2, 2), device=self.device)
         dummy_images = torch.rand(size=(1, 3, 32, 32), device=self.device)
-        self.generator(dummy_noise, dummy_images)
+        self.generator(dummy_images, dummy_noise)
+        self.automatic_optimization = False
 
-    def forward(self, noise, images):
-        return self.generator(noise, images)
+    def forward(self, images, noise):
+        return self.generator(images, noise)
 
     def adversarial_loss(self, y_hat, y):
         return torch.nn.BCELoss()(y_hat, y)
 
-    def training_step(self, batch, batch_idx, optimizer_idx):
+    def training_step(self, batch, batch_idx):
         real_imgs, _ = batch
         real_imgs = real_imgs.to(self.device)
         noise = torch.rand(real_imgs.size(0), 56, 2, 2, device=self.device)
-        fake_imgs = self(noise, real_imgs)
+        fake_imgs = self.generator(real_imgs, noise)
+
+        # Get the optimizers
+        opt_d, opt_g = self.optimizers()
 
         # Train discriminator
-        if optimizer_idx == 0:
-            real_loss = self.adversarial_loss(
-                self.discriminator(real_imgs),
-                torch.ones(real_imgs.size(0), 1, device=self.device),
-            )
-            fake_loss = self.adversarial_loss(
-                self.discriminator(fake_imgs.detach()),
-                torch.zeros(real_imgs.size(0), 1, device=self.device),
-            )
-            d_loss = (real_loss + fake_loss) / 2
-            self.log(
-                "d_loss",
-                d_loss,
-                on_step=True,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
-            )
-
-            # Log losses every 50 batches
-            if batch_idx % 50 == 0:
-                self.logger.experiment.log(
-                    {
-                        "losses/d_real": real_loss,
-                        "losses/d_fake": fake_loss,
-                        "losses/d": d_loss,
-                    }
-                )
-            return d_loss
+        opt_d.zero_grad()
+        real_loss = self.adversarial_loss(
+            self.discriminator(real_imgs),
+            torch.ones(real_imgs.size(0), 1, device=self.device),
+        )
+        fake_loss = self.adversarial_loss(
+            self.discriminator(fake_imgs.detach()),
+            torch.zeros(real_imgs.size(0), 1, device=self.device),
+        )
+        d_loss = (real_loss + fake_loss) / 2
+        self.log(
+            "d_loss",
+            d_loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        d_loss.backward()
+        opt_d.step()
 
         # Train generator
-        if optimizer_idx == 1:
-            g_loss = self.adversarial_loss(
-                self.discriminator(fake_imgs),
-                torch.ones(real_imgs.size(0), 1, device=self.device),
-            )
-            self.log(
-                "g_loss",
-                g_loss,
-                on_step=True,
-                on_epoch=True,
-                prog_bar=True,
-                logger=True,
+        opt_g.zero_grad()
+        g_loss = self.adversarial_loss(
+            self.discriminator(fake_imgs),
+            torch.ones(real_imgs.size(0), 1, device=self.device),
+        )
+        self.log(
+            "g_loss",
+            g_loss,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        g_loss.backward()
+        opt_g.step()
+
+        # Log losses every 50 batches
+        if batch_idx % 50 == 0:
+            self.logger.experiment.log(
+                {
+                    "losses/d_real": real_loss,
+                    "losses/d_fake": fake_loss,
+                    "losses/d": d_loss,
+                    "losses/g": g_loss,
+                }
             )
 
+        # Log generated images every 250 batches
         if batch_idx % 250 == 0:
             with torch.no_grad():
-                # Log generated images
                 img_grid_fake = torchvision.utils.make_grid(
                     fake_imgs[:16], normalize=True
+                )
+                img_grid_real = torchvision.utils.make_grid(
+                    real_imgs[:16], normalize=True
                 )
                 self.logger.experiment.log(
                     {
                         "images/generated": [
                             wandb.Image(img_grid_fake, caption="Generated Images")
-                        ]
+                        ],
+                        "images/real": [
+                            wandb.Image(img_grid_real, caption="Real Images")
+                        ],
                     }
                 )
-
-                # Log real images
-                img_grid_real = torchvision.utils.make_grid(
-                    real_imgs[:16], normalize=True
-                )
-                self.logger.experiment.log(
-                    {"images/real": [wandb.Image(img_grid_real, caption="Real Images")]}
-                )
-
-        return g_loss
 
     def configure_optimizers(self):
         lr = 0.0002
@@ -118,7 +123,9 @@ class GAN(pl.LightningModule):
         return [opt_d, opt_g], []
 
     def train_dataloader(self):
-        return get_cifar10_dataloader(self.target_class)
+        return get_cifar10_dataloader(
+            target_class=self.target_class, batch_size=self.batch_size, num_workers=8
+        )[0]
 
     def save_model_as_onnx(self, epoch, run_dir):
         run_dir = Path(run_dir)
@@ -140,6 +147,7 @@ class GAN(pl.LightningModule):
 def train(
     max_epochs: int = Option(500),
     wandb_run_name: str = Option("GAN-Simple-BCE-Experiment"),
+    batch_size: int = Option(64),
 ):
     current_time = datetime.now()
     session_name = current_time.strftime("%Y-%m-%d_%H-%M-%S")
@@ -172,7 +180,7 @@ def train(
         ],
     )
     # Initialize your GAN model
-    gan = GAN()
+    gan = GAN(batch_size=batch_size)
 
     # Start the training process
     trainer.fit(gan)
