06a69a69a3234a87399b2da79ee3f9f9578235fb
Zeynep Seda Birinci <zseda@gmail.com>
2024-03-04 21:48:44 +0100

    set seed

--- train.py
+++ train.py
@@ -10,8 +10,26 @@ from src.data import get_single_cifar10_dataloader as get_cifar10_dataloader
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
 from typer import Typer, Option
 from pathlib import Path
+import os
+import numpy as np
+import random
 
 app = Typer()
+
+
+# Function to set random seeds
+def set_random_seeds(seed_value=42):
+    random.seed(seed_value)  # Python random module
+    np.random.seed(seed_value)  # Numpy module
+    torch.manual_seed(seed_value)  # PyTorch
+    torch.cuda.manual_seed_all(seed_value)  # for multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed_value)
+
+
+# Set random seeds for reproducibility
+set_random_seeds(seed_value=42)
 
 
 # TODO: try different weight init methods
