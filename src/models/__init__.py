23d03a0ac5a5f76a31214402cc4acd55f91963d1
Zeynep Seda Birinci <zseda@gmail.com>
2024-03-04 18:15:22 +0100

    vanilla gan model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,5 @@ from ._generator import Generator
 from .model_dcgan import Generator as DCGenerator
 from .model_dcgan import Discriminator as DCDiscriminator
 from .model_dcgan import weights_init_normal
+from .vanilla_gan import Generator as VanillaGenerator
+from .vanilla_gan import Discriminator as VanillaDiscriminator
