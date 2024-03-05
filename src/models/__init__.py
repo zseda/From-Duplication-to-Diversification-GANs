66e4e4f8819d2f9debbe58775663e322b2e49cdb
Zeynep Seda Birinci <zseda@gmail.com>
2024-03-05 21:36:38 +0100

    fix imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,7 @@ from ._generator import Generator
 from .model_dcgan import Generator as DCGenerator
 from .model_dcgan import Discriminator as DCDiscriminator
 from .model_dcgan import weights_init_normal
+from .vanilla_gan import (
+    Generator as VanillaGenerator,
+    Discriminator as VanillaDiscriminator,
+)
