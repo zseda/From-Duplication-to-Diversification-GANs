622961dfcfb9228100bb90655b78ab0d7d20f6e2
Zeynep Seda Birinci <zseda@gmail.com>
2024-03-06 00:10:56 +0100

    linear layer

--- src/models/_discriminator.py
+++ src/models/_discriminator.py
@@ -21,7 +21,7 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         x = self.base_model(x)
-        x = torch.sigmoid(x)
+        # x = torch.sigmoid(x)
         return x
 
 
@@ -44,5 +44,5 @@ class DiscriminatorCustom(nn.Module):
 
     def forward(self, x):
         x = self.pipeline(x)
-        x = torch.sigmoid(x)
+        # x = torch.sigmoid(x)
         return x
